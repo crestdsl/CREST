b21b0e50dd27ef9101b3b96423ff90233fedae49
Stefan Klikovits <stefan.klikovits@unige.ch>
2018-02-08 17:43:21 +0100

    renamed validity to validator

--- src/model/validator.py
+++ src/model/validator.py
@@ -1,8 +1,10 @@
 import inspect
-import src.simulator.sourcehelper as SH
-from src.model.entity import *
+import types
 
-class ValidityCheck(object):
+import src.simulator.sourcehelper as SH
+from src.model import *
+
+class Validator(object):
 
     def __init__(self, model):
         if inspect.isclass(model):
@@ -15,6 +17,8 @@ class ValidityCheck(object):
         self.check_current_states()
         self.check_port_connections()
         self.test_parents_and_duplicate_usages()
+        self.check_update_sanity()
+        self.check_influence_sanity()
 
     def test_entity_hierarchy(self):
         """
@@ -67,7 +71,6 @@ class ValidityCheck(object):
             assert all_objs.count(obj) == 1, "Transition %s has been used multiple times" % obj._name
             assert obj._parent != None, "Transition %s has no parent definition" % obj._name
 
-
     def check_port_connections(self):
         all_ports = get_all_ports(self.model)
         influences_from_port = {p : [] for p in all_ports}
@@ -81,10 +84,9 @@ class ValidityCheck(object):
             influences_to_port[inf.target].append(inf)
 
         for up in get_all_updates(self.model):
-            for port in SH.get_written_ports_from_update(up):
-                updates_to_port[port].append(up)
+            updates_to_port[up.target].append(up)
 
-            for port in SH.get_read_ports_from_update(up):
+            for port in SH.get_read_ports_from_update(up.function, up):
                 updates_reading_port[port].append(up)
 
         # perform tests
@@ -102,3 +104,31 @@ class ValidityCheck(object):
         for inf in get_all_influences(self.model):
             assert inf.source in sources(inf._parent), "Specified influence source %s is not one of the entity's source ports" % inf.source
             assert inf.target in targets(inf._parent), "Specified influence target %s is not one of the entity's target ports" % inf.target
+
+    def check_update_sanity(self):
+        for update in get_all_updates(self.model):
+            entity = update._parent
+            assert update.target in targets(entity)
+
+            for port in SH.get_read_ports_from_update(up.function, up):
+                assert port in sources(entity)
+
+            assert type(update.function) is types.FunctionType
+            assert len(inspect.signature(update.function).parameters) == 2, "An update should have one argument 'dt' (besides 'self')"
+            assert 'dt' in inspect.signature(update.function).parameters
+
+    def check_influence_sanity(self):
+        for influence in get_all_influences(self.model):
+            entity = influence._parent
+            assert influence.source in sources(entity)
+            assert influence.target in targets(entity)
+            assert type(influence.function) is types.FunctionType
+            assert len(inspect.signature(influence.function).parameters) == 1, "An influence should not have arguments (except 'self')"
+
+    def check_transition_sanity(self):
+        for trans in get_all_transitions(self.model):
+            entity = trans._parent
+            assert trans.source in get_states(entity)
+            assert trans.target in get_states(entity)
+            assert type(trans.function) is types.FunctionType
+            assert len(inspect.signature(trans.function).parameters) == 1, "A transition should not have arguments (except self)"
