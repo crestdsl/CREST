d7343ee9ccc985efffefb3e2cea581e6bd734c0f
Stefan Klikovits <stefan.klikovits@unige.ch>
2018-02-28 17:21:41 +0100

    explicit imports to avoid problems

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,4 @@
-from .datatypes import *
+from .datatypes import Types, INT, INTEGER, FLOAT, REAL, STRING, BOOL
 from .model import State, Transition, Update, Influence, \
                    update, influence, transition  # decorators
 from .ports import Port, Input, Output, Local
