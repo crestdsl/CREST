a897d552607f14d5d215ec0efd95d670d0dafa15
Stefan Klikovits <stefan.klikovits@unige.ch>
2018-03-07 12:55:47 +0100

    renamed tests

---
+++ src/simulator/test/test_sourcehelper_ast.py
@@ -0,0 +1,42 @@
+import unittest
+import ast, astor
+import src.simulator.sourcehelper as SH
+from pprint import pprint
+
+import logging
+logging.basicConfig(level=logging.INFO)  # basic logging level
+
+class TestSourceHelper_ast(unittest.TestCase):
+
+    """ Tests """
+
+    def test_getast_of_function(self):
+        def func(self, dt):
+                y = 1
+            # zzz = "this should not be visible"
+                x = 22
+                if x < 30:
+                    y = 50
+                else:
+                    y = 100.5
+                y += 3
+                return y * 4
+
+        tree = SH.getast(func)
+        print(astor.dump_tree(tree))
+
+
+    def test_getast_of_lambda(self):
+        func = (lambda x : x)
+        y = func
+        tree = SH.getast(func)
+        print(astor.dump_tree(tree))
+        print(astor.dump_tree(SH.get_ast_from_lambda(func)))
+
+
+    def test_getast_of_lambda_with_comments(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
