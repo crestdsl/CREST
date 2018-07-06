6e03e85695845e177fa0ced791e61f74b401a391
Stefan Klikovits <stefan.klikovits@unige.ch>
2018-07-06 17:35:26 +0200

    explicit import from entity

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,4 +3,10 @@ from .model import State, Transition, Update, Influence, Action, \
     update, influence, transition, action  # decorators
 from .ports import Port, Input, Output, Local
 from .resource import Resource
-from .entity import *
+from .entity import Entity, add, \
+    get_parent, get_name, get_children, get_sources, get_targets, get_path_to_attribute, \
+    get_all_entities, get_all_influences, get_all_updates, get_all_ports, \
+    get_all_states, get_all_transitions, \
+    get_entities, get_states, get_inputs, get_outputs, get_locals, get_ports, get_actions, \
+    get_updates, get_transitions, get_influences, \
+    get_crest_objects
