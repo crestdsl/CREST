6daf05de013005fecbb97ca95b315422ca89a6bf
Stefan Klikovits <stefan.klikovits@unige.ch>
2019-02-27 00:36:45 +0100

    add an output for when colored library is not installed

--- crestdsl/simulation/interactivesimulator.py
+++ crestdsl/simulation/interactivesimulator.py
@@ -1,7 +1,11 @@
-from crestdsl.model import get_transitions
+from crestdsl.model import *  # bad practice, but used for the evaluation of commands
 from .simulator import Simulator
-import colored
-from colored import stylize
+try:
+    import colored
+    from colored import stylize
+    color_enabled = True
+except ImportError:
+    color_enabled = False
 import random
 import sys
 
@@ -18,16 +22,69 @@ class InteractiveSimulator(Simulator):
         if len(enabled_transitions) == 1:
             return enabled_transitions[0]
         elif len(enabled_transitions) > 1:
-            return self.prompt_transition_selection(entity, enabled_transitions)
+            if color_enabled:
+                return self.prompt_transition_selection(entity, enabled_transitions)
+            else:
+                return self.prompt_transition_selection_no_colored(entity, enabled_transitions)
         else:
             return None
+
+    def prompt_transition_selection_no_colored(self, entity, enabled_transitions):
+        pad = 1 if len(enabled_transitions) <= 10 else 2
+        transitions_texts = [idx.rjust(pad) + f"  ... {trans._name} (transition to '{trans.target._name}')" for idx, trans in enumerate(enabled_transitions)]
+        transitions_list = "\n".join(transitions_texts)
+
+        longtext = f"""
+ Non-Determinism detected
+There are multiple enabled transitions in entity: {str(entity)}
+(Current time: {self.global_time} -- Current automaton state: {entity.current._name})
+
+Choose one of the following transitions by entering the according number:
+{transitions_list}
+
+Other commands:
+r  ... choose a transition randomly
+p  ... plot the system
+pe ... plot the entity in which non-determinism occurs
+q! ... to exit the script (not recommended in Jupyter mode)
+
+Any other input will be interpreted.
+This means you can use it to e.g. inspect ports values.
+The entity {str(entity)} is bound to the variable 'entity'.
+Example: entity.my_port.value will print the value of port my_port.
+"""
+        print(longtext)
+        while True:
+            prompt = "Your choice: "
+            userinput = input(prompt).strip()  # read input
+            if userinput == "p":
+                self.plot()
+            elif userinput == "pe":
+                self.plot(entity=entity)
+            elif userinput == "r":
+                return random.choice(enabled_transitions)
+            elif userinput == "q!":
+                sys.exit()
+            elif userinput in [str(idx) for idx in range(len(enabled_transitions))]:
+                choice = int(userinput)
+                return enabled_transitions[choice]  # <<<<< This is the exit of the function, otherwise we're trapped !!
+            else:
+                try:
+                    print(eval(userinput))
+                except:
+                    text = f"I don't understand the input: " + \
+                             userinput + \
+                             f" (Please try again!)"
+                    print(text)
+
 
     def prompt_transition_selection(self, entity, enabled_transitions):
         pad = 1 if len(enabled_transitions) <= 10 else 2
         transitions_texts = [stylize(idx, colored.attr("bold")).rjust(pad) + f"  ... {trans._name} (transition to '{trans.target._name}')" for idx, trans in enumerate(enabled_transitions)]
         transitions_list = "\n".join(transitions_texts)
 
-        longtext = f"""{stylize(' Non-Determinism detected ', colored.fg('black') + colored.bg('dark_orange') + colored.attr('bold'))}
+        longtext = f"""
+{stylize(' Non-Determinism detected ', colored.fg('black') + colored.bg('dark_orange') + colored.attr('bold'))}
 There are multiple enabled transitions in entity: {stylize(' '+str(entity)+' ',  colored.fg('black') + colored.bg('yellow_1') + colored.attr('bold'))}
 (Current time: {stylize(self.global_time, colored.attr("bold"))} -- Current automaton state: {stylize(entity.current._name, colored.attr("bold"))})
 
@@ -40,6 +97,10 @@ There are multiple enabled transitions in entity: {stylize(' '+str(entity)+' ', 
 {stylize('pe', colored.attr("bold"))} ... plot the entity in which non-determinism occurs
 {stylize('q!', colored.attr("bold"))} ... to exit the script (not recommended in Jupyter mode)
 
+{stylize('Any other input will be interpreted.', colored.attr('underlined'))}
+This means you can use it to e.g. inspect ports values.
+The entity {stylize(str(entity), colored.attr('bold'))} is bound to the variable {stylize('entity',  colored.attr('bold'))}.
+{stylize('Example:', colored.attr('underlined'))} entity.my_port.value will print the value of port my_port.
 """
         print(longtext)
         while True:
@@ -57,4 +118,10 @@ There are multiple enabled transitions in entity: {stylize(' '+str(entity)+' ', 
                 choice = int(userinput)
                 return enabled_transitions[choice]  # <<<<< This is the exit of the function, otherwise we're trapped !!
             else:
-                print(stylize(f"I don't understand the input: {userinput}\nPlease try again!", colored.fg("red") + colored.attr("bold")))
+                try:
+                    print(eval(userinput))
+                except:
+                    text = stylize(f"I don't understand the input: ", colored.fg("red") + colored.attr("bold")) + \
+                             userinput + \
+                             stylize(f" (Please try again!)", colored.fg("red") + colored.attr("bold"))
+                    print(text)
