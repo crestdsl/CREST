138e044444e236ca3a4873a34995df2a76fe59c6
Stefan Klikovits <stefan.klikovits@unige.ch>
2019-04-01 02:20:51 +0200

    add default plotting

--- crestdsl/ui/__init__.py
+++ crestdsl/ui/__init__.py
@@ -1,2 +1,7 @@
 from .draw_statespace import draw_plotly as plotly_statespace
 from .draw_statespace import draw_plot as plot_statespace
+
+# zero-config plotting, just use whatever is default
+def plot(object,  name=''):
+    from crestdsl.config import config
+    return config.default_plotter.plot(object, name=name)
